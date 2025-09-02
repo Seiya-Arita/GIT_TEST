2fd6402621a8f81b4881109fe4fcda2327a21c45
Seiya-Arita <seiya.arita@dxc.com>
2025-09-02 13:38:58 +0900

    Update execute_all.sh

--- shell/execute_all.sh
+++ shell/execute_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_CSV="/home/ec2-user/kyo/Step2_TestList.csv"     # 入力CSVファイル名
+INPUT_CSV="/home/ec2-user/kyo/Step22_TestList.csv"     # 入力CSVファイル名
 OUTPUT_CSV="/home/ec2-user/kyo/script_results.csv"   # 出力CSVファイル名
 
 # 出力CSVのヘッダー
@@ -40,4 +40,5 @@ do
 
     rm -f "$stdout_file" "$stderr_file"
 done
+
 
